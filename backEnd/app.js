666d9fdac0e49d0074183e0eee8d6b1a551983de
cedsalvet <cedric.salvetat@gmail.com>
2023-03-17 10:41:28 +0100

    install dovenv to manage the environment variables

--- backEnd/app.js
+++ backEnd/app.js
@@ -6,9 +6,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
-const port = 8888
-
-
 const ticket_router = require('./routes/ticket')
 
 
@@ -18,4 +15,4 @@ app.use('/tickets', ticket_router)
 app.get('/', (req, res) => res.send('Hello World'))
 app.get('*', (req, res) => res.status(404).send(`Are you lost somewhere ?`))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`))
+app.listen(process.env.SERVER_PORT, () => console.log(`Example app listening on port ${process.env.SERVER_PORT}`))
