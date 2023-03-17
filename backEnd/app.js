51358b4405dd2568af3a67bdc41c2899d8b343c3
cedsalvet <cedric.salvetat@gmail.com>
2023-03-17 11:52:13 +0100

    npm i mysql2 sequelize
    init a mysql connection to store all tickets and comment
    next step models

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { Sequelize } = require('sequelize');
 
 const app = express()
 app.use(cors())
@@ -14,5 +15,22 @@ app.use('/tickets', ticket_router)
 
 app.get('/', (req, res) => res.send('Hello World'))
 app.get('*', (req, res) => res.status(404).send(`Are you lost somewhere ?`))
+
+
+
+
+let sequelize = new Sequelize(
+    process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false
+}
+)
+
+sequelize.authenticate()
+    .then( () => console.log('DB Connection OK'))
+    .catch( (err) => console.log('Database Error', err))
+
 
 app.listen(process.env.SERVER_PORT, () => console.log(`Example app listening on port ${process.env.SERVER_PORT}`))
